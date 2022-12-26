0714cdf3330aa7c840c9696af76205022c6c8c58
viettmab <viettmab123@gmail.com>
2022-12-26 16:59:02 +0700

    fix: change id

--- src/geo/geo.repository.ts
+++ src/geo/geo.repository.ts
@@ -8,7 +8,7 @@ export class GeoRepository {
   async getCountryByCode(code: string): Promise<DocumentType<Country> | null> {
     return CountryModel.findOne({ code })
       .select({
-        _id: 1,
+        _id: 0,
         code: 1,
         name: 1,
       })
@@ -18,7 +18,7 @@ export class GeoRepository {
   async getCityByCode(code: string): Promise<DocumentType<City> | null> {
     return CityModel.findOne({ code })
       .select({
-        _id: 1,
+        _id: 0,
         code: 1,
         name: 1,
         country_code: 1,
@@ -29,7 +29,7 @@ export class GeoRepository {
   async getCityByCountryCode(country_code: string) {
     return CityModel.find({ country_code })
       .select({
-        _id: 1,
+        _id: 0,
         code: 1,
         name: 1,
         country_code: 1,
@@ -42,7 +42,7 @@ export class GeoRepository {
   ): Promise<DocumentType<District> | null> {
     return DistrictModel.findOne({ code })
       .select({
-        _id: 1,
+        _id: 0,
         code: 1,
         name: 1,
         city_code: 1,
@@ -53,7 +53,7 @@ export class GeoRepository {
   async getDistrictByCityCode(city_code: string) {
     return DistrictModel.find({ city_code })
       .select({
-        _id: 1,
+        _id: 0,
         code: 1,
         name: 1,
         city_code: 1,
@@ -64,7 +64,7 @@ export class GeoRepository {
   async getWardByCode(code: string): Promise<DocumentType<District> | null> {
     return WardModel.findOne({ code })
       .select({
-        _id: 1,
+        _id: 0,
         code: 1,
         name: 1,
         district_code: 1,
@@ -75,7 +75,7 @@ export class GeoRepository {
   async getWardByDistrictCode(district_code: string) {
     return WardModel.find({ district_code })
       .select({
-        _id: 1,
+        _id: 0,
         code: 1,
         name: 1,
         district_code: 1,
